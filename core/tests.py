3941b69ed5560e497cf167601665c8584113c512
Kelvin Mutuku <mutukuk553@gmail.com>
2025-04-24 16:04:27 +0300

    Created and updated files in the core folder

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Client
 
-# Create your tests here.
+class ClientModelTest(TestCase):
+    def test_create_client(self):
+        client = Client.objects.create(full_name="John Doe", age=28, contact="0700000000")
+        self.assertEqual(client.full_name, "John Doe")
--- core/views.py
+++ core/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render
+from .forms import ClientForm, EnrollmentForm
 from .models import Client
+from django.shortcuts import render, get_object_or_404
+
+def add_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+    return render(request, 'core/add_client.html', {'form': form})
 
 def client_list(request):
-    query = request.GET.get('q')
-    clients = Client.objects.filter(full_name__icontains=query) if query else Client.objects.all()
-    return render(request, 'core/client_list.html', {'clients': clients})
+    clients = Client.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
+
+def enroll_client(request):
+    if request.method == 'POST':
+        form = EnrollmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = EnrollmentForm()
+    return render(request, 'core/enroll_client.html', {'form': form})
 
 def client_profile(request, client_id):
     client = get_object_or_404(Client, pk=client_id)
-    return render(request, 'core/client_profile.html', {'client': client})
+    return render(request, 'client_profile.html', {'client': client})
